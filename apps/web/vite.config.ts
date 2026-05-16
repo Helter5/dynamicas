6c9640a81dfcbd6cbc25003886be097ede69b162
Helter5 <g.kanocz2@icloud.com>
2026-05-16 22:59:04 +0200

    chore: remove useless url paths

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'node:path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/ferrari_wdc/final/dynamicas/',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
